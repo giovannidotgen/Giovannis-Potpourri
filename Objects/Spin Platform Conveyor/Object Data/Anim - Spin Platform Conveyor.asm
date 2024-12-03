baec5e2b5bb2200b7ea9a7f23b9969c11ff4ac21
TheBlad768 <master.metalsonic2014@gmail.com>
2024-12-03 05:51:07 +0400

    Object anim update

--- Objects/Spin Platform Conveyor/Object Data/Anim - Spin Platform Conveyor.asm
+++ Objects/Spin Platform Conveyor/Object Data/Anim - Spin Platform Conveyor.asm
@@ -7,5 +7,5 @@ Ani_SpinConvey: offsetTable
 		offsetTableEntry.w .still	; 1
 
 .spin	dc.b 0, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, $A, $B, $C, $D, $E, $F, 0, afEnd
-.still		dc.b $F, 0, afEnd
+.still		dc.b $7F, 0, afEnd
 	even
