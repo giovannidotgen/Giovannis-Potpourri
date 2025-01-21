3b64f1a16d394bafddd1dc07a7e0e2258bf4a858
Giovanni Battaglia <iamgiovanni.gb@gmail.com>
2025-01-21 21:22:03 +0100

    Dual reset on floor prevention

--- Objects/Sonic/Sonic.asm
+++ Objects/Sonic/Sonic.asm
@@ -2728,6 +2728,19 @@ loc_12158:
 ; =============== S U B R O U T I N E =======================================
 
 Player_TouchFloor:
+		cmpa.w	#Player_1,a1
+		bne.s	.isplayer2
+		btst	#0,(Player_ResetOnFloor).w
+		bne.w	locret_12230
+		bset	#0,(Player_ResetOnFloor).w
+		bra.s	.common
+	
+	.isplayer2:
+		btst	#1,(Player_ResetOnFloor).w
+		bne.w	locret_12230
+		bset	#1,(Player_ResetOnFloor).w
+	
+	.common:
 		cmpi.b	#PlayerID_Tails,character_id(a0)			; is player Tails?
 		beq.w	Tails_TouchFloor_Check_Spindash			; if yes, branch
 		cmpi.b	#PlayerID_Knuckles,character_id(a0)		; is player Knuckles?
