e2f6a93944b4743bf9a5b6e206d79ee4a35b7ca1
TheBlad768 <master.metalsonic2014@gmail.com>
2025-09-22 06:04:00 +0400

    Perform_DPLC for Shields

--- Objects/Players/Shields/Object Data/DPLC - Blue Shield.asm
+++ Objects/Players/Shields/Object Data/DPLC - Blue Shield.asm
@@ -9,16 +9,16 @@ DPLC_BlueShield_4: 	dc.w DPLC_BlueShield_14-DPLC_BlueShield
 DPLC_BlueShield_6: 	dc.w DPLC_BlueShield_18-DPLC_BlueShield
 DPLC_BlueShield_8: 	dc.w DPLC_BlueShield_1C-DPLC_BlueShield
 DPLC_BlueShield_A: 	dc.w DPLC_BlueShield_20-DPLC_BlueShield
-DPLC_BlueShield_C: 	dc.b $0, $1
-	dc.b $30, $0
-DPLC_BlueShield_10: 	dc.b $0, $1
-	dc.b $30, $4
-DPLC_BlueShield_14: 	dc.b $0, $1
-	dc.b $30, $8
-DPLC_BlueShield_18: 	dc.b $0, $1
-	dc.b $30, $C
-DPLC_BlueShield_1C: 	dc.b $0, $1
-	dc.b $30, $10
-DPLC_BlueShield_20: 	dc.b $0, $1
-	dc.b $B0, $14
+DPLC_BlueShield_C: 	dc.b $0, $0
+	dc.b $0, $3
+DPLC_BlueShield_10: 	dc.b $0, $0
+	dc.b $0, $43
+DPLC_BlueShield_14: 	dc.b $0, $0
+	dc.b $0, $83
+DPLC_BlueShield_18: 	dc.b $0, $0
+	dc.b $0, $C3
+DPLC_BlueShield_1C: 	dc.b $0, $0
+	dc.b $1, $3
+DPLC_BlueShield_20: 	dc.b $0, $0
+	dc.b $1, $4B
 	even
